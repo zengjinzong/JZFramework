0c5915bfca819abd1421664ef6a20ad2cea0f1a8
zengjinzong <980224860@qq.com>
2020-03-05 10:14:40 +0800

    项目恢复

---
+++ zjz/zjz/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  zjz
+//
+//  Created by 曾进宗 on 2020/3/4.
+//  Copyright © 2020 zengjinzong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ zjz/zjz/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  zjz
+//
+//  Created by 曾进宗 on 2020/3/4.
+//  Copyright © 2020 zengjinzong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
